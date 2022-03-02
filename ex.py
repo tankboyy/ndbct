e09c258d8209b2322ba0a257e0ab337c1a6a6d9d
ksj <tankboya@naver.com>
2022-03-02 20:02:20 +0900

    왕실의 나이트 ex

--- ex.py
+++ ex.py
@@ -1,13 +1,17 @@
 # N 입력받기
-n = int(input())
+n = input()
+
+row = int(n[1])
+column = int(ord(n[0])) - int(ord('a')) + 1
+
+        # 좌 위, 좌 아래, 아래 좌, 아래 우, 우 아래, 우 위, 위 우, 위 좌
+moves = [(-2, -1), (-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2), (-1, -2)]
+
 count = 0
 
-for h in range(n + 1):
-    
-    for m in range(60):
 
-        for s in range(60):
-            if '3' in str(h) + str(m) + str(s):
-                count += 1
-
+for move in moves:
+    if row + move[0] > 0:
+        if column + move[1] > 0:
+            count += 1
 print(count)
