3bd208cdb6baac734f24f651e28bbdf14a4209c1
ksj <tankboya@naver.com>
2022-03-07 12:22:00 +0900

    요세푸스 queue, rotate

---
+++ 요세푸스.py
@@ -0,0 +1,12 @@
+from collections import deque
+
+N, K = map(int, input().split())
+
+dq = deque(range(1, N + 1))
+ans = list()
+
+while len(dq):
+    dq.rotate(-K + 1)
+    ans.append(dq.popleft())
+
+print(f"<{str(ans)[1:-1]}>")
